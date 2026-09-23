a82a59458385b33a4fed41b108ca60af7101126f
Pushkar Bopanwar <pushkar.bopanwar@npstx.com>
2026-09-23 11:01:53 +0530

    Initial BookHive project setup

---
+++ BookhiveBackend/src/test/java/com/example/BookhiveBackend/BookhiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookhiveBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhiveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
